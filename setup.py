6299bcc80e323f5432a16ce458e6aa86e3be700e
markalos <goknowy@gmail.com>
2018-04-26 23:12:21 +0800

    add sub pixel fitting

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.2',  # Required
+    version='0.1.6',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -175,6 +175,7 @@ setup(
     entry_points={  # Optional
         'console_scripts': [
             'maximumInscribedCircle=MaximumInscribedCircle.MaximumInscribedCircle:main',
+            'vectorUtil=MaximumInscribedCircle.VectorUtil:main'
         ],
     },
 
